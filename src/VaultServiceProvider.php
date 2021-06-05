4481ca6766e01ba3dc16e7c378c1128e3f4df860
Daniel Koch <daniel.koch@publicare.de>
2021-06-05 19:31:35 +0200

    initial setup

---
+++ src/VaultServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace DionTech\Vault;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class VaultServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+}
