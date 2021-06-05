ad72e520aaf9c30ade52883a769e9fc7bff75dd1
Daniel Koch <daniel.koch@publicare.de>
2021-06-05 19:53:16 +0200

    a first wy of implement encrypt decrypt values

--- src/Models/Secret.php
+++ src/Models/Secret.php
@@ -4,9 +4,14 @@
 namespace DionTech\Vault\Models;
 
 
+use DionTech\Vault\Support\Casts\EncrypterDecrypter;
 use Illuminate\Database\Eloquent\Model;
 
 class Secret extends Model
 {
     protected $fillable = ['vault_id', 'alias', 'value'];
+
+    protected $casts = [
+        'value' => EncrypterDecrypter::class
+    ];
 }
