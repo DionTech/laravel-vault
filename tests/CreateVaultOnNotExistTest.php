7bf012bcfac338788efe8752520d495721bf331f
Daniel Koch <daniel.koch@publicare.de>
2021-06-18 12:54:29 +0200

    when same command follows, test is failing ause instead of using existing one, at the moment we will stupidly create again one

--- tests/CreateVaultOnNotExistTest.php
+++ tests/CreateVaultOnNotExistTest.php
@@ -14,6 +14,12 @@ class CreateVaultOnNotExistTest extends TestCase
 {
     public function test_creating_on_not_exists_when_adding_secret()
     {
+        //when narg is a string, it will becreated
+        \DionTech\Vault\Support\Facades\Vault::open("test_vault");
+
+        $this->assertEquals(1,Vault::count());
+
+        //when opening again, it must NOT be created again
         \DionTech\Vault\Support\Facades\Vault::open("test_vault");
 
         $this->assertEquals(1,Vault::count());
