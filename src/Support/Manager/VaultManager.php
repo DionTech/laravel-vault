cb244c0d3b71ff46fbe6ba801b66e4ab5aaa2d04
Daniel Koch <daniel.koch@publicare.de>
2021-06-18 12:52:28 +0200

    added now a create function if vault is a string

--- src/Support/Manager/VaultManager.php
+++ src/Support/Manager/VaultManager.php
@@ -6,18 +6,41 @@ namespace DionTech\Vault\Support\Manager;
 
 use DionTech\Vault\Models\Vault;
 use DionTech\Vault\Support\Contracts\VaultServiceContract;
+use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class VaultManager
+ * @package DionTech\Vault\Support\Manager
+ */
 class VaultManager
 {
+    /**
+     * @var string|mixed
+     */
     protected string $key;
 
+    /**
+     * @var
+     */
     protected $vault;
 
+    /**
+     * @var
+     */
+    protected $context;
+
+    /**
+     * VaultManager constructor.
+     */
     public function __construct()
     {
         $this->key = env("APP_KEY");
     }
 
+    /**
+     * @param string $key
+     * @return $this
+     */
     public function useKey(string $key): VaultManager
     {
         $this->key = $key;
@@ -25,13 +48,25 @@ class VaultManager
         return $this;
     }
 
-    public function open(Vault $vault): VaultManager
+    /**
+     * @param Vault|string $vault
+     * @return $this
+     */
+    public function open($vault): VaultManager
     {
+        if (is_string($vault)) {
+            return $this->createVault($vault);
+        }
+
         $this->vault = $vault;
 
         return $this;
     }
 
+    /**
+     * @param string $secret
+     * @return string
+     */
     public function get(string $secret): string
     {
         return app()->makeWith(VaultServiceContract::class, [
@@ -39,6 +74,10 @@ class VaultManager
         ])->getSecret($this->vault, $secret);
     }
 
+    /**
+     * @param string $secret
+     * @param string $value
+     */
     public function add(string $secret, string $value): void
     {
         app()->makeWith(VaultServiceContract::class, [
@@ -46,10 +85,38 @@ class VaultManager
         ])->addSecret($this->vault, $secret, $value);
     }
 
+    /**
+     * @param string $secret
+     * @param string $value
+     */
     public function overwrite(string $secret, string $value): void
     {
         app()->makeWith(VaultServiceContract::class, [
             'key' => $this->key
         ])->overwriteSecret($this->vault, $secret, $value);
+    }
+
+    /**
+     * @param mixed $context
+     * @return VaultManager
+     */
+    public function setContext($context)
+    {
+        $this->context = $context;
+        return $this;
+    }
+
+    protected function createVault(string $vault): VaultManager
+    {
+        $params = ['name' => $vault];
+
+        if ($this->context instanceof Model) {
+            $params['model_id'] = $this->context->model_id;
+            $params['model_type'] = get_class($this->context);
+        }
+
+        $this->vault = Vault::create($params);
+
+        return $this;
     }
 }
